9ea176fbfc8eb6996051f6fc3cc2ed7e27f746aa
RipanRenaldi25 <ripanrenaldi25042002@gmail.com>
2026-09-18 19:34:14 +0700

    test(CommentItemComponent): Create unit test for comment item component

---
+++ src/components/tests/CommentInput.test.jsx
@@ -0,0 +1,53 @@
+/**
+ * Should render component correctly
+ * Should handle comment typing event correctly
+ * Should call onSubmti function when submit button clicked
+ */
+
+import React, { useState } from 'react';
+import { screen, render, cleanup } from '@testing-library/react';
+import { afterEach, describe, expect, it, vi } from 'vitest';
+import '@testing-library/jest-dom/vitest';
+import CommentInput from '../CommentInput';
+import userEvent from '@testing-library/user-event';
+
+describe('CommentInput Component', () => {
+  afterEach(() => {
+    cleanup();
+  });
+
+  it('Should render component correctly', async () => {
+    render(<CommentInput commentValue={''} onChangeCommentValue={() => {}} onSubmitHandler={() => {}}/>);
+
+    const heading = await screen.getByRole('heading', {
+      level: 2,
+      name: 'Berikan komentar'
+    });
+
+    expect(heading).toBeVisible();
+  });
+
+  it('Should handle comment typing event correctly', async () => {
+    const onChangeHandler = vi.fn();
+    render(<CommentInput commentValue={''} onChangeCommentValue={onChangeHandler} onSubmitHandler={() => {}}/>);
+
+    const commentInput = screen.getByRole('textbox');
+
+    await userEvent.type(commentInput, 'Contoh Comment');
+
+    expect(onChangeHandler).toHaveBeenCalledTimes('Contoh Comment'.length);
+  });
+
+  it('Should call onSubmti function when submit button clicked', async () => {
+    const onSubmit = vi.fn();
+    render(<CommentInput commentValue={''} onChangeCommentValue={() => {}} onSubmitHandler={onSubmit}/>);
+
+    const submitButton = await screen.getByRole('button', {
+      name: 'Kirim'
+    });
+
+    await userEvent.click(submitButton);
+
+    expect(onSubmit).toHaveBeenCalled();
+  });
+});
