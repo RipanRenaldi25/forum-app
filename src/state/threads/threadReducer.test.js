926f23f2e4e6bc4eb3e45ece1bad39495dabb516
RipanRenaldi25 <ripanrenaldi25042002@gmail.com>
2026-09-25 23:23:31 +0700

    test(ThreadReducer): Add unit test for upvote thread

--- src/state/threads/threadReducer.test.js
+++ src/state/threads/threadReducer.test.js
@@ -2,12 +2,13 @@
  * Should return new state when given by action FETCH_THREAD
  * Should add new thread when adding a new thread
  * Should return unique category of all threads
+ * Should add upvote to thread when given by action UPVOTE_THREAD
  */
 
 import { describe, expect, it } from 'vitest';
 import threadReducer from './threadReducer';
 import ActionType from './ActionType';
-import { createThreadActionCreator, filterThreadActionCreator, getThreadsActionCreator } from './Action';
+import { createThreadActionCreator, filterThreadActionCreator, getThreadsActionCreator, upVoteThreadActionCreator } from './Action';
 
 
 describe('Thread Reducer', () => {
@@ -95,5 +96,39 @@ describe('Thread Reducer', () => {
     });
 
 
+  });
+
+  it('Should add upvote to thread when given by action UPVOTE_THREAD', () => {
+    const initialThread = {
+      threads: [
+        {
+          id: 1,
+          title: 'Hello',
+          body: 'This is body test',
+          upvotesBy: []
+        },
+        {
+          id: 2,
+          title: 'Hello2',
+          body: 'This is body test2',
+          upvotesBy: []
+        }
+      ],
+    };
+
+    const initialState = {
+      threads: initialThread.threads,
+      category: []
+    };
+
+    const nextState = threadReducer(initialState, upVoteThreadActionCreator(1, 'user-1'));
+
+    expect(nextState).toEqual({
+      ...initialState,
+      threads: initialState.threads.map((thread) => ({
+        ...thread,
+        upvotesBy: thread.id === 1 ? [...thread.upvotesBy, 'user-1'] : thread.upvotesBy
+      }))
+    });
   });
 });
